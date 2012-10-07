7032838f50d70cbbb91a4661741c7848479f9a6d
Lucas Sampaio <magal.lucas@gmail.com>
2012-10-07 19:32:31 -0300

    First commit =D

---
+++ problems/1/problem-1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import doctest
+
+
+def sum_divisors(n):
+    """
+    Solution for Problem 1 of Project Euler
+    http://projecteuler.net/problem=1
+
+    >>> sum_divisors(10)
+    23
+    """
+    sequence = (x for x in xrange(1, n) if x % 3 == 0 or x % 5 == 0)
+    return reduce(lambda x, y: x + y, sequence)
+
+
+if __name__ == '__main__':
+    doctest.testmod()
